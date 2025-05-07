26d50f88653508fbeb4bfe26c74d599c3e7d0173
Marcelino Ibrahim <marcelino.saad@proton.me>
2025-05-08 01:50:01 +0300

    open application edit and delete

--- src/components/logged/JobApplications.jsx
+++ src/components/logged/JobApplications.jsx
@@ -443,12 +443,12 @@ const JobApplications = () => {
             {appStatusError && <p className="text-red-500 text-sm">{appStatusError}</p>}
 
             <label className="text-md font-medium text-vngrey2 mt-5">Date</label>
-            <input
+            {/* <input
               type="date"
-              value={appDate.split("T")[0]}
+              value={appDate.toISOString().split('T')[0]}
               onChange={(e) => handleDateChange(new Date(e.target.value))}
               className={`w-full p-2 border text-vnblack1 rounded-md ${appDateError ? 'border-red-500' : 'border-gray-300'}`}
-            />
+            /> */}
             {appDateError && <p className="text-red-500 text-sm">{appDateError}</p>}
 
             <label className="text-md font-medium text-vngrey2 mt-5">Notes</label>
