1a7fcfb87a27c433b958bc9ac960138a1268a97f
Marcelino Ibrahim <marcelino.saad@proton.me>
2025-05-09 21:28:02 +0300

    job recommend

--- src/components/logged/JobRecommendation.jsx
+++ src/components/logged/JobRecommendation.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import Context from '../../Context';
+import { use } from 'react';
 
 const JobRecommendation = () => {
   const [foundJobs, setFoundJobs] = useState([])
@@ -7,46 +8,55 @@ const JobRecommendation = () => {
     userData,
   } = useContext(Context);
 
-  async function fetchJobs(jobTitle) {
-    const response = await fetch("https://nour83-job.hf.space/get-jobs", {
-      method: "POST",
+  async function fetchJobs(userData) {
+    const splitJobTitle = userData.jobTitle.split(' ');
+    const jobTitle = splitJobTitle.join('%20');
+    const url = `https://jsearch.p.rapidapi.com/search?query=${jobTitle}&date_posted=week`;
+    const options = {
+      method: 'GET',
       headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ job_title: jobTitle })
-    });
-  
-    const data = await response.json();
-    return data.jobs;
+        'x-rapidapi-key': '62b39a02c1mshdfb3cfafe57d9d3p191ccajsn22b3dc592be1',
+        'x-rapidapi-host': 'jsearch.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+    return result;
   }
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const jobTitle = userData.jobTitle || "Software Engineer"; // Default to "Software Engineer" if jobTitle is not available
-      const jobs = await fetchJobs(jobTitle);
-      setFoundJobs(jobs);
-    }
-    fetchData();
-  }, [userData.jobTitle]);
-  
+    useEffect(() => {
+      if (!userData.jobTitle) return;
+      const fetchData = async () => {
+        const jobs = await fetchJobs(userData);
+        setFoundJobs(jobs);
+      }
+      fetchData();
+    }, [userData]);
 
-  return (
-    <div className='w-full min-h-screen flex flex-col lg:flex-row justify-start items-center lg:justify-center lg:items-start gap-5 p-4'>
-      <div className='w-11/12 flex flex-wrap items-center justify-center gap-4 p-4'>
-      <h1 className='w-full text-primary text-2xl text-center'>Job Applications</h1>
-      {foundJobs.length > 0 ? (
-          foundJobs.map((job, index) => (
-            <div key={index} className='w-11/12 sm:w-5/12 flex flex-col items-start justify-start gap-3 p-4 bg-vnbg shadow-md rounded-lg hover:shadow-lg transition duration-300'>
-              <h2 className='text-xl font-bold text-primary mb-3'>{job.title}</h2>
-              <button>{job.link}</button>              
-            </div>
-          ))
-        ) : (
-          <p className='text-2xl text-vnblack1'>No recommended jobs found.</p>
-        )}
-      </div>
-      </div>
-  )
-}
 
-export default JobRecommendation
+    return (
+      <div className='w-full min-h-screen flex flex-col lg:flex-row justify-start items-center lg:justify-center lg:items-start gap-5 p-4'>
+        <div className='w-11/12 flex flex-wrap items-center justify-center gap-4 p-4'>
+          <h1 className='w-full text-primary text-2xl text-center'>Job Applications</h1>
+          {foundJobs.length > 0 ? (
+            foundJobs.map((job, index) => (
+              <div key={index} className='w-11/12 sm:w-5/12 flex flex-col items-start justify-start gap-3 p-4 bg-vnbg shadow-md rounded-lg hover:shadow-lg transition duration-300'>
+                <h2 className='text-xl font-bold text-primary mb-3'>{job.title}</h2>
+                <button>{job.link}</button>
+              </div>
+            ))
+          ) : (
+            <p className='text-2xl text-vnblack1'>No recommended jobs found.</p>
+          )}
+        </div>
+      </div>
+    )
+  }
+
+  export default JobRecommendation
