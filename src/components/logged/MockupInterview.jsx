56feafaf7cc53d4b2f4213a94cf786784893c5a5
Marcelino Ibrahim <marcelino.saad@proton.me>
2025-05-12 01:17:53 +0300

    interview integration done

--- src/components/logged/MockupInterview.jsx
+++ src/components/logged/MockupInterview.jsx
@@ -1,12 +1,72 @@
 import { useContext, useEffect, useState } from 'react'
 import Context from '../../Context'
+import { Button } from '@material-tailwind/react';
+import { MdError } from 'react-icons/md';
 
 const MockupInterview = () => {
-  const [questions, setQuestions] = useState([])
+  const [questions, setQuestions] = useState(0)
+  const [actButton, setActButton] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const [error, setError] = useState("")
+  const [started, setStarted] = useState(false)
+
+  const [curQuestion, setCurQuestion] = useState(0)
+  const [curAnswer, setCurAnswer] = useState("")
+  const [answerError, setAnswerError] = useState("")
+  const [actButton2, setActButton2] = useState(false)
+
+  const [feedbacks, setFeedbacks] = useState([])
 
   const {
     userData,
   } = useContext(Context)
+
+  //Activate Button
+  useEffect(() => {
+    if (userData.jobTitle) {
+      setActButton(true)
+    } else {
+      setActButton(false)
+    }
+  }, [userData])
+
+  //Activate Loading
+  useEffect(() => {
+    if (questions === 0){
+      setLoading(true)
+    } else {
+      setLoading(false)
+    }
+  }, [questions])
+
+  //Activate Answer Button
+  useEffect(() => {
+    if (curAnswer === "" || answerError !== "") {
+      setActButton2(false)
+    } else {
+      setActButton2(true)
+    }
+  }, [curAnswer, answerError])
+
+  const handleChangeAnswer = (val) => {
+    if (val === "") {
+      setAnswerError("Please provide an answer.");
+    } else if (val.trim().length < 30) {
+      setAnswerError("Please provide a more detailed answer.");
+    } else if (/^([a-zA-Z])\1{4,}$/.test(val.trim())) {
+      setAnswerError("Your answer contains repeated characters. Try writing something meaningful.");
+    } else if (/^[0-9\s!@#$%^&*(),.?\":{}|<>[\]\\\-=_+`~]+$/.test(val.trim())) {
+      setAnswerError("Answer must include words, not just numbers or symbols.");
+    } else if (!/[a-zA-Z]/.test(val.trim())) {
+      setAnswerError("Answer must contain at least one letter.");
+    } else if (val.trim() === val.trim().toUpperCase() && /[A-Z]/.test(val.trim())) {
+      setAnswerError("Avoid writing in all caps.");
+    } else {
+      setAnswerError("");
+      setCurAnswer(val)
+    }
+  }
 
   // Function to fetch mockup interview questions from the API
   const fetchQuestions = async () => {
@@ -19,21 +79,91 @@ const MockupInterview = () => {
         body: JSON.stringify({
           job_role: userData.jobTitle,
         })
-    })
+      })
       if (!response.ok) {
         throw new Error('Network response was not ok')
       }
       const data = await response.json()
-      setQuestions(data)
-      console.log('Mockup interview questions:', data)
+      setQuestions(data.questions)
+      setStarted(true)
+      setError('')
+
     } catch (error) {
-      console.error('Error fetching questions:', error)
+      setError('Failed to get questions. Please try again later.')
     }
   }
 
-  fetchQuestions()
+  // Function to fetch the next question
+  const fetchNextQuestion = async () => {
+    try {
+      const response = await fetch(import.meta.env.VITE_INTERVIEW_API + "/submit-answer", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          answer: curAnswer,
+          question: questions[curQuestion]
+        })
+      })
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      const data = await response.json()
+      setFeedbacks([...feedbacks, {...data, question: questions[curQuestion], answer: curAnswer }])
+      setCurQuestion(curQuestion + 1)
+      setCurAnswer("")
+    } catch (error) {
+      setError('Failed to submit answer. Please try again later.')
+    }
+  }
+
   return (
-    <div>MockupInterview</div>
+    <div className='w-full min-h-screen flex flex-col justify-start items-center gap-5 p-4'>
+      <div className='w-11/12 flex flex-wrap items-center justify-center gap-4 p-4'>
+        <h1 className='w-full text-primary text-2xl text-center'>Mock-Up Interview</h1>
+        {error != "" && <p className='w-full text-center text-lg text-red-500'>{error}</p>}
+        <Button onClick={() => fetchQuestions()} disabled={!actButton} className={`${started ? "hidden" : "flex"} w-11/12 font-medium normal-case flex-row items-center justify-center text-xl bg-primary text-vnwhite rounded-lg hover:bg-vngrey3 transition duration-300 ease-in-out`}>
+          Start Interview
+        </Button>
+        {started && 
+          loading ? 
+            (<p className='text-2xl text-vnblack1'>Loading...</p>) : 
+            (questions.length > 0 ? 
+              (
+                <div className='w-11/12 flex flex-col items-center justify-center gap-4 p-4 border-vngrey4 border rounded-lg'>
+                  <h2 className='w-full text-vnblack1 text-xl text-center'>{questions[curQuestion]}</h2>
+                  {answerError != "" && <p className='flex flex-row gap-2 items-center text-start w-full text-red-500 text-sm -mb-5'><MdError />{answerError}</p>}
+                  <textarea
+                    value={curAnswer}
+                    onChange={(e) => handleChangeAnswer(e.target.value)}
+                    className="w-full p-2 border text-vnblack1 rounded-md min-h-40"
+                    rows="4"
+                  ></textarea>
+                  <Button onClick={() => fetchNextQuestion()} disabled={!actButton2} className={`${questions.length === curQuestion ? "hidden" : "flex"} w-11/12 font-medium normal-case flex-row items-center justify-center text-xl bg-primary text-vnwhite rounded-lg hover:bg-vngrey3 transition duration-300 ease-in-out`}>
+                    Submit
+                  </Button>
+                  {questions.length === curQuestion && <p className='text-2xl text-success'>You have completed the interview!</p>}
+                </div>
+              ) :
+              (<p className='text-2xl text-vnblack1'>No questions found.</p>)
+            )
+        }
+        {feedbacks.length > 0 && 
+          <div className='w-11/12 flex flex-col items-center justify-center gap-4 p-4 border-vngrey4 border rounded-lg'>
+            <h2 className='w-full text-vnblack1 text-xl text-center'>Feedback</h2>
+            {feedbacks.map((feedback, index) => (
+              <div key={index} className='w-full flex flex-col items-start justify-start gap-2 p-4 border-vngrey4 border rounded-lg'>
+                <p className='text-success text-lg'>Question: {feedback.question}</p>
+                <p className='text-success text-sm'>Answer: {feedback.answer}</p>
+                <p className='text-success text-sm'>Feedback: {feedback.feedback}</p>
+                <p className='text-success text-sm'>Rate: {feedback.rating}</p>
+              </div>
+            ))}
+          </div>
+        }
+      </div>
+    </div>
   )
 }
 
