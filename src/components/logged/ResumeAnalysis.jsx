e84ded3392cbc746d924d515c540460d02d2e710
Marcelino Ibrahim <marcelino.saad@proton.me>
2025-05-13 12:40:56 +0300

    resume analyze

--- src/components/logged/ResumeAnalysis.jsx
+++ src/components/logged/ResumeAnalysis.jsx
@@ -1,47 +1,109 @@
-import React, { useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import Context from '../../Context'
+import { Button, Spinner } from '@material-tailwind/react';
+import { MdError } from 'react-icons/md';
 
-const ResumeAnalysis = () => {
-  const [resume, setResume] = useState(null)
-  const [resumeError, setResumeError] = useState("")
+const MockupInterview = () => {
+  const [actButton, setActButton] = useState(false)
+  const [loading, setLoading] = useState(true)
 
-  const handleFileChange = (event) => {
-    const file = event.target.files[0]
-    if (file && file.type === 'application/pdf') {
-      setResume(file)
+  const [error, setError] = useState("")
+
+  const [curFile, setCurFile] = useState([])
+
+  const [feedback, setFeedback] = useState(null)
+
+  const {
+    userData,
+  } = useContext(Context)
+
+  //Activate Button
+  useEffect(() => {
+    if (userData.jobTitle) {
+      setLoading(false)
+      if (curFile) {
+        setActButton(true)
+      } else {
+        setActButton(false)
+      }
     } else {
-      setResumeError("Please select a valid PDF file")
+      setLoading(true)
     }
-  }
+  }, [userData])
 
-  const handleResumeAnalysis = async () => {
-    const formData = new FormData()
-    formData.append('file', resume)
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setCurFile(file);
+  };
 
+  // Function to fetch mockup interview questions from the API
+  const analyzeResume = async () => {
+    setLoading(true)
     try {
-        fetch(import.meta.env.VITE_ANALYSIS_API +  '/analyze', {
-          method: 'POST',
-          body: formData
+      const response = await fetch(import.meta.env.VITE_ANALYSIS_API + "/analyze_resume", {
+        method: 'POST',
+        body: JSON.stringify({
+          job_role: userData.jobTitle,
         })
-        .then(response => response.json())
-        .then(data => console.log(data.feedback));
-      console.log("Resume analyzed successfully")
-    } catch (error) {
-      setResumeError("Error analyzing resume. Please try again.")
-    }
-  }
-  return (
-    <div className='w-full min-h-screen flex flex-col lg:flex-row justify-start items-center lg:justify-center lg:items-start gap-5 p-4'>
-      <div className='w-11/12 lg:w-2/3 flex flex-wrap items-center justify-center gap-4 p-4'>
-        <h1 className='text-2xl font-bold text-center'>Resume Analysis</h1>
-        {resumeError && <p className='text-red-500'>{resumeError}</p>}
-        <p className='text-center'>Upload your resume and get instant feedback on how to improve it.</p>
-        <p className='text-center'>Accept only PDF</p>
-        <input onChange={handleFileChange} type="file" accept=".pdf" className='border border-gray-300 rounded p-2' />
-        <button disabled={resume ? false : true} onClick={handleResumeAnalysis} className='bg-blue-500 text-white rounded p-2'>Analyze</button>
+      })
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      const data = await response.json()
+      console.log(data)
+      setFeedback(data)
+      setError('')
 
+    } catch (error) {
+      setError('Failed to get questions. Please try again later.')
+    }
+    setLoading(false)
+  }
+
+  return (
+    <div className='w-full min-h-screen flex flex-col justify-start items-center gap-5 p-4'>
+      <div className='w-11/12 flex flex-wrap items-center justify-center gap-4 p-4'>
+        <h1 className='w-full text-primary text-2xl text-center'>Resume Analyze</h1>
+        {error != "" && <p className='w-full text-center text-lg text-red-500'>{error}</p>}
+        {loading ?
+            (
+              <div className='w-full lg:w-11/12 flex flex-col items-center justify-center gap-4 p-4'>
+                <p className='text-2xl text-vnblack1 text-center w-full'>Loading...</p>
+                <Spinner className="h-16 w-16 text-primary" />
+              </div>
+            ) :
+            (
+              <div className='w-full lg:w-11/12 flex flex-col items-center justify-center gap-4 p-4 border-vngrey4 border rounded-lg'>
+                <h2 className={`${questions.length === curQuestion ? "hidden" : "block"} w-full text-vnblack1 text-xl text-center`}>{questions[curQuestion]}</h2>
+                {error != "" && <p className='flex flex-row gap-2 items-center text-start w-full text-red-500 text-sm -mb-3'><MdError />{error}</p>}
+                <input
+                  type='file'
+                  accept='.pdf'
+                  value={curFile}
+                  onChange={handleFileChange}
+                  className={`w-full p-2 border text-vnblack1 rounded-md min-h-40`}
+                />
+                <Button onClick={() => analyzeResume()} disabled={!actButton} className={`flex w-11/12 font-medium normal-case flex-row items-center justify-center text-xl bg-primary text-vnwhite rounded-lg hover:bg-vngrey3 transition duration-300 ease-in-out`}>
+                  Analyze Resume
+                </Button>
+                {questions.length === curQuestion && <p className='text-2xl text-success text-center w-full'>You have completed the interview!</p>}
+              </div>
+            )
+        }
+        {feedback &&
+          <div className='w-full lg:w-11/12 flex flex-col items-center justify-center gap-4 p-4 border-vngrey4 border rounded-lg'>
+            <h2 className='w-full text-vnblack1 text-xl text-center'>Feedback</h2>
+              <div className='w-full flex flex-col items-start justify-start gap-2 p-4 border-vngrey4 border rounded-lg'>
+                <p className='text-success w-full text-sm lg:text-lg'>Question: <span className='text-vnblack2'>1</span></p>
+                <p className='text-success w-full text-sm lg:text-base'>Answer: <span className='text-vnblack2'>2</span></p>
+                <p className='text-success w-full text-sm lg:text-xl'>Feedback: <span className='text-vnblack2'>3</span></p>
+                <p className='text-success w-full text-sm lg:text-xl'>Rate: <span className='text-vnblack2'>    4</span></p>
+              </div>
+          </div>
+        }
       </div>
     </div>
   )
 }
 
-export default ResumeAnalysis
+export default MockupInterview
